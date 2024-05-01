ccbe2a6ad10d462777224c47e4dde1f4357bdf37
fevoritos <fevoritos@mail.ru>
2024-05-01 22:28:10 +0300

    new

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,62 +1,63 @@
-import './App.css'
-import Button from './components/Button/Button'
-import Header from './components/Header/Header';
-import CardButton from './components/CardButton/CardButton';
-import JournalItem from './components/JournalItem/JournalItem';
-import JournalList from './components/JournalList/JournalList';
-import Body from './layouts/Body/Body';
-import LeftPanel from './layouts/LeftPanel/LeftPanel';
-import JournalAddButton from './components/JournalAddButton/JournalAddButton';
-import { useState } from 'react';
-import JournalForm from './components/JournalForm/JournalForm';
+import "./App.css";
+import Header from "./components/Header/Header";
+import CardButton from "./components/CardButton/CardButton";
+import JournalItem from "./components/JournalItem/JournalItem";
+import JournalList from "./components/JournalList/JournalList";
+import Body from "./layouts/Body/Body";
+import LeftPanel from "./layouts/LeftPanel/LeftPanel";
+import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
+import { useState } from "react";
+import JournalForm from "./components/JournalForm/JournalForm";
+
+const INITIAL_DATA = [
+    {
+        title: "Подготовка к обновлению курсов",
+        text: "Горные походы открывают удивительные природные ландшафты",
+        date: new Date(),
+    },
+    {
+        title: "Поход в горы",
+        text: "Думал, что очень много времени",
+        date: new Date(),
+    },
+];
 
 function App() {
-	const data = [
-		{
-			title: 'Подготовка к обновлению курсов',
-			text: 'Горные походы открывают удивительные природные ландшафты',
-			date: new Date(),
+    const [items, setItems] = useState(INITIAL_DATA);
 
-		},
-		{
-			title: 'Поход в горы',
-			text: 'Думал, что очень много времени',
-			date: new Date(),
+    const addItem = (item) => {
+        setItems((oldItems) => [
+            ...oldItems,
+            {
+                text: item.text,
+                title: item.title,
+                date: new Date(item.date),
+            },
+        ]);
+    };
 
-		}
-	];
-	
-
-	return (
-		<div className='app'>
-			<LeftPanel>
-				<Header />
-				<JournalAddButton></JournalAddButton>
-				<JournalList>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							text={data[0].text}
-							date={data[0].date}
-						/>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							text={data[1].text}
-							date={data[1].date}
-
-						/>
-					</CardButton>
-				</JournalList>
-			</LeftPanel>
-			<Body>
-				<JournalForm/>
-			</Body>
-				
-
-		</div>
-	);
+    return (
+        <div className="app">
+            <LeftPanel>
+                <Header />
+                <JournalAddButton></JournalAddButton>
+                <JournalList>
+                    {items.map((el) => (
+                        <CardButton>
+                            <JournalItem
+                                title={el.title}
+                                text={el.text}
+                                date={el.date}
+                            />
+                        </CardButton>
+                    ))}
+                </JournalList>
+            </LeftPanel>
+            <Body>
+                <JournalForm onSubmit={addItem} />
+            </Body>
+        </div>
+    );
 }
 
-export default App
+export default App;
