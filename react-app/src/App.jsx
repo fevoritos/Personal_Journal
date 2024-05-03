a744b45c32b5c5c44f37cdc1f0ae90d42cb2a87f
fevoritos <fevoritos@mail.ru>
2024-05-03 14:37:19 +0300

    key

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -11,11 +11,13 @@ import JournalForm from "./components/JournalForm/JournalForm";
 
 const INITIAL_DATA = [
     {
+        id: 1,
         title: "Подготовка к обновлению курсов",
         text: "Горные походы открывают удивительные природные ландшафты",
         date: new Date(),
     },
     {
+        id: 2,
         title: "Поход в горы",
         text: "Думал, что очень много времени",
         date: new Date(),
@@ -32,8 +34,17 @@ function App() {
                 text: item.text,
                 title: item.title,
                 date: new Date(item.date),
+                id: Math.max(...oldItems.map((i) => i.id)) + 1,
             },
         ]);
+    };
+
+    const sortItems = (a, b) => {
+        if (a.date < b.date) {
+            return 1;
+        } else {
+            return -1;
+        }
     };
 
     return (
@@ -42,8 +53,8 @@ function App() {
                 <Header />
                 <JournalAddButton></JournalAddButton>
                 <JournalList>
-                    {items.map((el) => (
-                        <CardButton>
+                    {items.sort(sortItems).map((el) => (
+                        <CardButton key={el.id}>
                             <JournalItem
                                 title={el.title}
                                 text={el.text}
