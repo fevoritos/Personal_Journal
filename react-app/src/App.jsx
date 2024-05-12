a9c9a398a312a7a14f5bfd2777920678262ff5e4
fevoritos <fevoritos@mail.ru>
2024-05-12 21:31:25 +0300

    update

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -7,6 +7,7 @@ import JournalAddButton from "./components/JournalAddButton/JournalAddButton";
 import JournalForm from "./components/JournalForm/JournalForm";
 import { useLocalStorage } from "./hooks/use-localstorage.hook";
 import { UserContextProvider } from "./context/user.context";
+import { useState } from "react";
 
 // const INITIAL_DATA = [
 // {
@@ -35,16 +36,30 @@ function mapItems(items) {
 
 function App() {
     const [items, setItems] = useLocalStorage(["data"]);
+    const [selectedItem, setSelectedItem] = useState({});
 
     const addItem = (item) => {
-        setItems([
-            ...mapItems(items),
-            {
-                ...item,
-                date: new Date(item.date),
-                id: items.length > 0 ? Math.max(...items.map((i) => i.id)) + 1 : 1,
-            },
-        ]);
+        if (!item.id) {
+            setItems([
+                ...mapItems(items),
+                {
+                    ...item,
+                    date: new Date(item.date),
+                    id: items.length > 0 ? Math.max(...items.map((i) => i.id)) + 1 : 1,
+                },
+            ]);
+        } else {
+            setItems([
+                ...mapItems(items).map((i) => {
+                    if (i.id === item.id) {
+                        return {
+                            ...item,
+                        };
+                    }
+                    return i;
+                }),
+            ]);
+        }
     };
 
     return (
@@ -53,10 +68,10 @@ function App() {
                 <LeftPanel>
                     <Header />
                     <JournalAddButton />
-                    <JournalList items={mapItems(items)} />
+                    <JournalList items={mapItems(items)} setItem={setSelectedItem} />
                 </LeftPanel>
                 <Body>
-                    <JournalForm onSubmit={addItem} />
+                    <JournalForm onSubmit={addItem} data={selectedItem} />
                 </Body>
             </div>
         </UserContextProvider>
